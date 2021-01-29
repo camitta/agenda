9e2104b96901bb7d9b7942d5552fb1b0d5dce8dc
Ako Sorensen <akosorensen@gmail.com>
2021-01-29 16:32:17 -0500

    feat: added association seeds

--- script/seed-data/index.js
+++ script/seed-data/index.js
@@ -3,11 +3,13 @@ const seedBoards = require('./board-seed')
 const seedTasks = require('./task-seed')
 const seedChecklistItems = require('./checklist-item-seed')
 const seedMantra = require('./mantra-seed')
+const seedAssociations = require('./association-seed')
 
 module.exports = {
   seedUsers,
   seedBoards,
   seedTasks,
   seedChecklistItems,
-  seedMantra
+  seedMantra,
+  seedAssociations
 }
