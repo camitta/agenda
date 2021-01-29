ff2842444ba9dc4d72aa6dc5cf6f93a121664b15
Allyson Camitta <allyson.camitta@gmail.com>
2021-01-29 16:00:59 -0500

    add api routes for user board view

--- server/api/boards.js
+++ server/api/boards.js
@@ -1,7 +1,31 @@
 const router = require('express').Router()
-const {Board} = require('./db/models/board')
+const {Board, User} = require('./db/models')
 module.exports = router
 
 //GET /boards
 
-//GET /boards/:boardId
+//GET api/boards/:boardId
+router.get('/:boardId', async (req, res, next) => {
+  try {
+    if (req.user) {
+      const board = await Board.findOne({
+        where: {
+          userId: req.user.id
+        },
+        include: {model: Task}
+      })
+      if (board) {
+        const task = await Task.findAll({
+          where: {
+            boardId: board.id
+          }
+        })
+        res.json(task)
+      }
+    } else if (!req.user) {
+      res.send(401)
+    }
+  } catch (err) {
+    next(err)
+  }
+})
---
+++ server/api/tasks.js
@@ -0,0 +1,3 @@
+const router = require('express').Router()
+const {Board, User} = require('./db/models/board')
+module.exports = router
