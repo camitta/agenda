876490c10528dc6b08b28b116174827ef2101677
Allyson Camitta <allyson.camitta@gmail.com>
2021-01-29 13:20:26 -0500

    create boards api file

---
+++ server/api/boards.js
@@ -0,0 +1,9 @@
+const router = require('express').Router()
+const {Board} = require('./db/models/board')
+module.exports = router
+
+//GET /boards
+
+//GET /boards/:boardId
+
+module.exports = router
