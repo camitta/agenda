1d17cab72e2e78252df8094f3d3d4ccb51c10ba5
Allyson Camitta <allyson.camitta@gmail.com>
2021-01-29 17:37:06 -0500

    add board components

--- client/components/single-board.js
+++ client/components/single-board.js
@@ -1,1 +1,4 @@
 import React, {Component} from 'react'
+import Task from './single-board'
+import Grid from '@material-ui/core/Grid'
+import Box from '@material-ui/core/Box'
--- client/components/task.js
+++ client/components/task.js
@@ -1,1 +1,57 @@
 import React, {Component} from 'react'
+import Box from '@material-ui/core/Box'
+
+class Task extends React.Component {
+  constructor() {
+    super()
+  }
+
+  componentDidMount() {
+    try {
+      this.props.fetchTasks(this.props.match.params.songId)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  // handleClick(songId) {
+  // 	const currentMixtape = this.props.cart[0];
+  // 	if (currentMixtape) {
+  // 		this.props.addSongToCart(songId, currentMixtape.id);
+  // 		toast.notify('Added to cart!', {
+  // 			position: 'top-right'
+  // 		});
+  // 	} else {
+  // 		this.props.createOrder();
+  // 		this.props.addSongToCart(songId, currentMixtape.id);
+  // 	}
+  // }
+
+  render() {
+    const task = this.props.task
+    return (
+      <Box bgcolor="primary.main" color="primary.contrastText" p={2}>
+        primary.main
+      </Box>
+    )
+  }
+}
+
+// const mapStateToProps = state => {
+// 	return {
+// 		song: state.single_song,
+// 		cart: state.cartReducer
+// 	};
+// };
+
+// const mapDispatchToProps = dispatch => {
+// 	return {
+// 		fetchSingleSong: id => dispatch(fetchSingleSong(id)),
+// 		loadCart: () => dispatch(fetchCart()),
+// 		addSongToCart: (songId, mixtapeId) => dispatch(addSongToCart(songId, mixtapeId)),
+// 		createOrder: () => dispatch(createNewOrder())
+// 	};
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Single_Song);
+export default Task
