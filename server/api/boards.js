2a6a09ff901ee3b864dd10715220699be75c25eb
Kathy Morawski <kathymorawski@gmail.com>
2021-01-31 12:41:51 -0600

    Feat: Added route for new users to be added to a baord

--- server/api/boards.js
+++ server/api/boards.js
@@ -7,7 +7,9 @@ const {Board, User} = require('../db/models')
 router.get('/:boardId', async (req, res, next) => {
   try {
     if (req.user) {
-      const board = await Board.findByPk(req.params.boardId)
+      const board = await Board.findByPk(req.params.boardId, {
+        include: User
+      })
       res.json(board)
     } else if (!req.user) {
       res.send(401)
@@ -59,6 +61,33 @@ router.delete('/delete/:boardId', async (req, res, next) => {
       }
     })
     res.send(204).end()
+  } catch (err) {
+    next(err)
+  }
+})
+
+//add new user to board
+// api/boards/:boardId/add/user
+router.put('/:boardId/add/user', async (req, res, next) => {
+  try {
+    const board = await Board.findOne({
+      where: {
+        id: req.params.boardId
+      }
+    })
+    //our req.body.email will be the person being invited
+    const user = await User.findOne({
+      where: {
+        email: req.body.email
+      }
+    })
+    if (user) {
+      await board.addUser(user)
+      res.send(204).end()
+    } else if (!user) {
+      // for now if the person's email entered is not signed up on our site, send a 404 'Not found' error. We will need to add this in the front end component so the user inviting their friend knows why the invite is not working
+      res.send(404)
+    }
   } catch (err) {
     next(err)
   }
