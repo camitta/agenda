5c7df08d1d5e650e2633b4d9d79e8990ec0691ef
Allyson Camitta <allyson.camitta@gmail.com>
2021-02-01 10:44:39 -0500

    add all-tasks to redux store

--- client/store/tasks.js
+++ client/store/tasks.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 /**
  * ACTION TYPES
  */
-const GET_TASKS = 'GET_TASKS'
+
 const GET_SINGLE_TASK = 'GET_SINGLE_TASK'
 const REMOVE_SINGLE_TASK = 'REMOVE_SINGLE_TASK'
 const ADD_SINGLE_TASK = 'ADD_SINGLE_TASK'
@@ -17,7 +17,6 @@ const initialState = {}
  * ACTION CREATORS
  */
 
-// const fetchTasks = tasks => ({type: GET_TASKS, tasks})
 const fetchSingleTask = singleTask => ({type: GET_SINGLE_TASK, singleTask})
 const removeSingleTask = () => ({type: REMOVE_SINGLE_TASK})
 const addedSingleTask = singleTask => ({type: ADD_SINGLE_TASK, singleTask})
@@ -25,16 +24,6 @@ const editedSingleTask = singleTask => ({type: EDIT_SINGLE_TASK, singleTask})
 /**
  * THUNK CREATORS
  */
-
-//Do we need a getTasks? Started to write one out but not sure
-// export const getTasks = (boardId) => async dispatch => {
-//   try {
-//     const {data} = await axios.get(`/api/tasks/${boardId}`)
-//     dispatch(fetchTasks(data))
-//   } catch (err) {
-//     console.log(err)
-//   }
-// }
 
 export const getSingleTask = (taskId, boardId) => async dispatch => {
   try {
@@ -82,8 +71,6 @@ export const editSingleTask = (id, task) => {
  */
 export default function(state = initialState, action) {
   switch (action.type) {
-    case GET_TASKS:
-      return action.tasks
     case GET_SINGLE_TASK:
       return action.singleTask
     case REMOVE_SINGLE_TASK:
