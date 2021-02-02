ef12f6df12de8dfa10bd5a51188ff91b1d2d83cf
Allyson Camitta <allyson.camitta@gmail.com>
2021-02-02 10:25:11 -0500

    ac/add edit button

--- client/components/task.js
+++ client/components/task.js
@@ -3,10 +3,11 @@ import {connect} from 'react-redux'
 import Card from '@material-ui/core/Card'
 import IconButton from '@material-ui/core/Button'
 import DeleteIcon from '@material-ui/icons/Delete'
+import EditIcon from '@material-ui/icons/Edit'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
 import styled from 'styled-components'
-import {deleteSingleTask} from '../store/tasks'
+import {deleteSingleTask, editSingleTask} from '../store/tasks'
 import {getAllTasks} from '../store/all-tasks'
 
 const TaskContainer = styled.div`
@@ -19,19 +20,26 @@ const Task = props => {
     await props.removeSingleTask(id)
     await props.getAllTasks(props.boardId)
   }
+  const handleEdit = async (id, task) => {
+    await props.updateSingleTask(id, task)
+    await props.getAllTasks(props.boardId)
+  }
 
   const task = props.task
 
   return (
     <TaskContainer>
       <Card>
-        <IconButton aria-label="delete" onClick={() => handleDelete(task.id)}>
-          <DeleteIcon />
-        </IconButton>
         <CardContent>
           <Typography>{task.description}</Typography>
         </CardContent>
       </Card>
+      <IconButton aria-label="delete" onClick={() => handleDelete(task.id)}>
+        <DeleteIcon />
+      </IconButton>
+      <IconButton aria-label="edit" onClick={() => handleEdit(task.id, task)}>
+        <EditIcon />
+      </IconButton>
     </TaskContainer>
   )
 }
@@ -39,7 +47,8 @@ const Task = props => {
 const mapDispatch = dispatch => {
   return {
     removeSingleTask: id => dispatch(deleteSingleTask(id)),
-    getAllTasks: boardId => dispatch(getAllTasks(boardId))
+    getAllTasks: boardId => dispatch(getAllTasks(boardId)),
+    updateSingleTask: (id, task) => dispatch(editSingleTask(id, task))
   }
 }
 
