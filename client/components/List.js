074f09d777bcd6dcea3f177309c59c2a29c4ac6c
Allyson Camitta <allyson.camitta@gmail.com>
2021-02-02 14:55:04 -0500

    ac/center single board

--- client/components/List.js
+++ client/components/List.js
@@ -7,6 +7,8 @@ import {connect} from 'react-redux'
 const ListContainer = styled.div`
   background-color: #dfe3e6;
   border-radius: 3px;
+  align-content: center;
+  justify-content: center;
   width: 300px;
   padding: 8px;
   height: 100%;
@@ -20,7 +22,9 @@ const List = props => {
       <Typography>{props.type}</Typography>
       {tasks &&
         tasks.length &&
-        tasks.map(task => <Task key={task.id} task={task} boardId={props.boardId}  />)}
+        tasks.map(task => (
+          <Task key={task.id} task={task} boardId={props.boardId} />
+        ))}
     </ListContainer>
   )
 }
