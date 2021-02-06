8cc111f0ee11035b372394c86a79ad6291f201d6
Ako Sorensen <akosorensen@gmail.com>
2021-02-05 19:49:50 -0500

    fix(AddUserToTask): added handleClose to remove a bug

--- client/components/UserAvatar.js
+++ client/components/UserAvatar.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {unassignUserFromTask} from '../store/tasks'
-import {connect} from 'react-redux'
-import Avatar from '@material-ui/core/Avatar'
 import {getAllTasks} from '../store/all-tasks'
+import {connect} from 'react-redux'
+
+// Material UI
+import Avatar from '@material-ui/core/Avatar'
+import Button from '@material-ui/core/Button'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
 
 const UserAvatar = props => {
   const {users, taskId, boardId} = props || []
+
+  const [open, setOpen] = useState(false)
+
+  const handleClickOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
 
   const handleDelete = async userId => {
     try {
@@ -20,10 +37,36 @@ const UserAvatar = props => {
     <>
       {users.length
         ? users.map(user => (
-            <Avatar key={user.id} onClick={() => handleDelete(user.id)}>
-              {user.firstName[0]}
-              {user.lastName[0]}
-            </Avatar>
+            <div key={user.id}>
+              <Avatar onClick={() => handleClickOpen()}>
+                {user.firstName[0]}
+                {user.lastName[0]}
+              </Avatar>
+              {/* <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-describedby="alert-dialog-description"
+              >
+                <DialogContent>
+                  <DialogContentText id="alert-dialog-description">
+                    Are you sure you want to remove {user.firstName} from the
+                    task?
+                  </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleClose} color="primary">
+                    Changed My Mind
+                  </Button>
+                  <Button
+                    onClick={() => handleDelete(user.id)}
+                    color="primary"
+                    autoFocus
+                  >
+                    Yes I Do
+                  </Button>
+                </DialogActions>
+              </Dialog> */}
+            </div>
           ))
         : null}
     </>
