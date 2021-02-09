e6d74c03b2a1aedb76e8e1115d18f6099eb6e3b0
Allyson Camitta <allyson.camitta@gmail.com>
2021-02-08 19:28:52 -0500

    Refactor(Theme) create separate dark theme

--- client/theme/theme.js
+++ client/theme/theme.js
@@ -74,37 +74,27 @@ const theme = createMuiTheme({
     }
   },
   palette: {
-    common: {
-      black: '#000',
-      white: '#fff'
-    },
-    background: {
-      paper: '#fff',
-      default: '#fafafa'
-    },
+    type: 'light',
     primary: {
-      light: 'rgba(195, 241, 207, 1)',
-      main: '#000',
-      dark: 'rgba(0, 56, 51, 1)',
-      contrastText: '#fff'
+      main: '#000000'
     },
     secondary: {
-      light: 'rgba(252, 248, 244, 1)',
-      main: 'rgba(187, 161, 193, 1)',
-      dark: 'rgba(98, 18, 68, 1)',
-      contrastText: 'rgba(255, 255, 255, 1)'
+      main: '#FEF6D3'
     },
     error: {
-      light: '#e57373',
-      main: 'rgba(193, 2, 48, 1)',
-      dark: 'rgba(193, 2, 48, 1)',
-      contrastText: '#fff'
+      main: '#C10230',
+      dark: '#C10230',
+      contrastText: '#ffffff'
     },
     text: {
-      primary: 'rgba(0, 0, 0, 0.87)',
-      secondary: 'rgba(0, 0, 0, 0.54)',
-      disabled: 'rgba(0, 0, 0, 0.38)',
-      hint: 'rgba(0, 0, 0, 0.38)'
+      primary: '#000000',
+      secondary: '#000000',
+      disabled: '#A69B95',
+      hint: '#FABA91'
+    },
+    background: {
+      paper: '#ffffff',
+      default: '#ffffff'
     }
   },
   overrides: {
