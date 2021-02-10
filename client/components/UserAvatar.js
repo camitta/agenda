8902589b380863ab3a9caa6de3ca7f0ad654ee43
Ako Sorensen <akosorensen@gmail.com>
2021-02-10 17:08:51 -0500

    fix: fixed a bug to remove user from task

--- client/components/UserAvatar.js
+++ client/components/UserAvatar.js
@@ -1,14 +1,24 @@
 import React, {useState} from 'react'
-import {DeleteUser} from './index'
 import {connect} from 'react-redux'
 import {unassignUserFromTask} from '../store/tasks'
 import {getAllTasks} from '../store/all-tasks'
 
+import Avatar from '@material-ui/core/Avatar'
+import Button from '@material-ui/core/Button'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+
 const UserAvatar = props => {
   const {users, taskId, boardId} = props || []
   const [open, setOpen] = useState(false)
+  const [userId, setUserId] = useState(null)
+  const [firstName, setFirstName] = useState('')
 
-  const handleClickOpen = () => {
+  const handleClickOpen = (clickedId, clickedFirstName) => {
+    setUserId(clickedId)
+    setFirstName(clickedFirstName)
     setOpen(true)
   }
 
@@ -16,7 +26,7 @@ const UserAvatar = props => {
     setOpen(false)
   }
 
-  const handleDelete = async userId => {
+  const handleDelete = async () => {
     try {
       await props.unassignUser(taskId, boardId, userId)
       await props.fetchTasks(boardId)
@@ -30,18 +40,34 @@ const UserAvatar = props => {
     <>
       {users.length
         ? users.map(user => (
-            <DeleteUser
+            <Avatar
               key={user.id}
-              userId={user.id}
-              firstName={user.firstName}
-              lastName={user.lastName}
-              handleClickOpen={handleClickOpen}
-              handleClose={handleClose}
-              handleDelete={handleDelete}
-              open={open}
-            />
+              onClick={() => handleClickOpen(user.id, user.firstName)}
+            >
+              {user.firstName[0]}
+              {user.lastName[0]}
+            </Avatar>
           ))
         : null}
+      <Dialog open={open} onClose={handleClose}>
+        <DialogContent>
+          <DialogContentText>
+            Are you sure you want to remove {firstName} from this task?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} style={{color: 'red'}}>
+            No
+          </Button>
+          <Button
+            onClick={() => handleDelete(userId)}
+            style={{color: 'green'}}
+            autoFocus
+          >
+            Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
   )
 }
