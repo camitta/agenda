87bed2ab819fd180bc9338ec45c8f1afa24c6179
Kathy Morawski <kathymorawski@gmail.com>
2021-02-13 15:58:32 -0600

    chore: add notes

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,5 +1,7 @@
 const Task = require('../db/models/index')
 
+//BACKEND
+
 module.exports = io => {
   io.on('connection', socket => {
     console.log(`A socket connection to the server has been made: ${socket.id}`)
